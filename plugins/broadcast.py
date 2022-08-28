66808326b4d74d1885d1e3440a070eab3a9970a6
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-08-28 16:02:11 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -30,7 +30,7 @@ from helper.database import getid
 id_pattern = re.compile(r'^.\d+$')
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]               
-PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "900873119 5756883599 5172114510").split()))
+PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "1323557247").split()))
 PRO_USERS.append(ADMIN)
 
 
