aa0a262e45e7a23a5b42518e93fe2698f9ff53d0
Agunivers7 <herokuone7@gmail.com>
2022-10-15 12:05:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DB_NAME = os.environ.get("DB_NAME","gowtham")
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://Aadhi:42426840@cluster0.h9rky.mongodb.net/?retryWrites=true&w=majority")
  
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://jvxgctfh:1Jf49IZA-vGumNNojNaLeOHCnMC6yfRn@tyke.db.elephantsql.com/jvxgctfh") 
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://apex:aadhi@8.219.177.252:5432/apexdb") 
 
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
