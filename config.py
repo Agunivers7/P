294c5ebe8543bd021db7aac71de399616818ebb0
Agunivers7 <herokuone7@gmail.com>
2022-10-20 16:03:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "5743481582:AAE54d0xnry2lX80YbaoEPQ4RM20
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Aguniversmovie") 
 
-DB_NAME = os.environ.get("DB_NAME","gowtham")     
+DB_NAME = os.environ.get("DB_NAME","xxxtrntasisns")     
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://Aadhi:42426840@cluster0.jqzpafx.mongodb.net/?retryWrites=true&w=majority")
  
