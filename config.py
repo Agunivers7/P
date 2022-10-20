3e74122f70ea10eaef6733b0ed26825dec9894b7
Agunivers7 <herokuone7@gmail.com>
2022-10-20 13:07:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import re, os
 
-id_pattern = re.compile(r'^.\d+$') 
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
-API_ID = os.environ.get("API_ID", "10651048")
-
-API_HASH = os.environ.get("API_HASH", "37775aca7d11f450ecde375baac17fe7")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "2103300889:AAEVZd_cAFHdJDU-hAYOdYtwALFwACETP2A") 
+# Bot information
+API_ID = int(environ.get('API_ID', '10651048'))
+API_HASH = environ.get('API_HASH', '37775aca7d11f450ecde375baac17fe7')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5473562541:AAGiYVUx1fNiXLs_KiTKoH9pJ0fAhYXUhK0')
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Aguniversmovie") 
 
