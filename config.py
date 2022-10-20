7b1395082e1ae0227306db6cfe8029b0f43f8d44
Agunivers7 <herokuone7@gmail.com>
2022-10-20 15:47:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "Aguniversmovie")
 
 DB_NAME = os.environ.get("DB_NAME","gowtham")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Aadhi:42426840@cluster0.h9rky.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Aadhi:42426840@cluster0.jqzpafx.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/e7aba0e06227ffd054728.jpg")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/a6fe48dd57f2a7f929c28.jpg")
 
 ADMIN = os.environ.get("ADMIN", "1323557247") 
