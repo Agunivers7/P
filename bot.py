f75da2eb221c184ee5e789c02a57c016b34bd20e
Agunivers7 <herokuone7@gmail.com>
2022-10-22 20:03:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
+            name=NAME, 
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
