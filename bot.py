5c5a6fc5de40d3e57a0f62943d7b5fae7c2a7a6e
Agunivers7 <herokuone7@gmail.com>
2022-10-22 20:01:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
