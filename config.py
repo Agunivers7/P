1a6dcd195beef1ce62b2e8e8ded3d6707c5c17c8
Agunivers7 <herokuone7@gmail.com>
2022-10-22 19:46:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = 10651048
 
 API_HASH = os.environ.get("API_HASH", "37775aca7d11f450ecde375baac17fe7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5694266322:AAFuhovA-kFby3QNJjTlGUttDxWtYUNdYuA") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5743646572:AAEn0QWcJtEMN3EbRRj_rNoshdhRZfqrKb8") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Aguniversmovie") 
 
@@ -21,3 +21,5 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/a6fe48dd57f2a7f929c28.jpg")
 
 ADMIN = os.environ.get("ADMIN", "1323557247") 
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://ihgmjbqi:Tx3okQ2JOQsW2iA4Pxiap71rk7viDi0n@lucky.db.elephantsql.com/ihgmjbqi") 
